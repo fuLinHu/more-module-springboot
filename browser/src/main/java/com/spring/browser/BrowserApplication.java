a4b0b3713f02edad5e978799896147824aee06e0
fulinhu <1205668006@qq.com>
2018-11-23 15:37:44 +0800

    多模块初步整合成功

--- browser/src/main/java/com/spring/browser/BrowserApplication.java
+++ browser/src/main/java/com/spring/browser/BrowserApplication.java
@@ -3,10 +3,10 @@ package com.spring.browser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class BrowserApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BrowserApplication.class, args);
-    }
+    //public static void main(String[] args) {
+        //SpringApplication.run(BrowserApplication.class, args);
+    //}
 }
