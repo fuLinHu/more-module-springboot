16d5a358cb4e3979b314d2eaff63ffde45289e5e
fulinhu <1205668006@qq.com>
2018-11-29 10:16:41 +0800

    公司提交-wiremock

---
+++ demo/src/main/java/com/spring/demo/wiremock/MockServer.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.spring.demo.wiremock;
+
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+/**
+ * @author zhailiang
+ *
+ */
+public class MockServer {
+
+	/**
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+		configureFor(8081);
+		removeAllMappings();
+
+		mock("/order/1", "01");
+		mock("/order/2", "02");
+	}
+
+	private static void mock(String url, String file) throws IOException {
+		ClassPathResource resource = new ClassPathResource("mock/response/" + file + ".txt");
+		String content = StringUtils.join(FileUtils.readLines(resource.getFile(), "UTF-8").toArray(), "\n");
+		stubFor(get(urlPathEqualTo(url)).willReturn(aResponse().withBody(content).withStatus(200)));
+	}
+
+}
