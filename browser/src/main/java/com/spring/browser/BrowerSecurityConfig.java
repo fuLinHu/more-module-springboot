27c86059fadaf108cd9efacc97e709ce5fabc6bf
fuLinHu <1205668006@qq.com>
2018-12-06 01:24:02 +0800

    在家-第四章结束

--- browser/src/main/java/com/spring/browser/BrowerSecurityConfig.java
+++ browser/src/main/java/com/spring/browser/BrowerSecurityConfig.java
@@ -1,8 +1,11 @@
 package com.spring.browser;
 
+import com.spring.core.authentication.AbstractChannelSecurityConfig;
+import com.spring.core.authentication.mobile.SmsCodeAuthenticationSecurityConfig;
 import com.spring.core.properties.SecurityConstants;
 //import com.spring.core.properties.SecurityProperties;
 import com.spring.core.properties.SecurityProperties;
+import com.spring.core.validate.code.ValidateCodeSecurityConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -10,14 +13,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 @Configuration
 @Slf4j
-public class BrowerSecurityConfig extends WebSecurityConfigurerAdapter {
+public class BrowerSecurityConfig extends AbstractChannelSecurityConfig {
 
     @Autowired
     private SecurityProperties securityProperties;
@@ -28,6 +36,16 @@ public class BrowerSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     protected AuthenticationFailureHandler imoocAuthenticationFailureHandler;
 
+    @Autowired
+    private ValidateCodeSecurityConfig validateCodeSecurityConfig;
+
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private SmsCodeAuthenticationSecurityConfig smsCodeAuthenticationSecurityConfig;
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
@@ -37,20 +55,31 @@ public class BrowerSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        applyPasswordAuthenticationConfig(http);
         //log.debug("Using default configure(HttpSecurity). If subclassed this will potentially override subclass configure(HttpSecurity).");
         //设置登录,注销，表单登录不用拦截，其他请求要拦截
-        http.formLogin()
+       /* formLogin()
                 //.loginPage("/imooc-signIn.html")//自定义的登录页面
                 .loginPage("/authentication/require") //跳转到一个controller层，然后在那里面进行控制跳转
                 .loginProcessingUrl("/authentication/form") //可以任意写，不用实现，但必须和登录表单post、请求action对应
                 .successHandler(imoocAuthenticationSuccessHandler)//登录成功的处理
-                .failureHandler(imoocAuthenticationFailureHandler)//登录成功的处理
+                .failureHandler(imoocAuthenticationFailureHandler)//登录成功的处理*/
+            http.apply(validateCodeSecurityConfig)
+                .and()
+                .apply(smsCodeAuthenticationSecurityConfig)
+                .and()
+                .rememberMe()
+                .tokenRepository(persistentTokenRepository())
+                .tokenValiditySeconds(securityProperties.getBrowser().getRememberMeSeconds())
+                .userDetailsService(userDetailsService)
                 .and()
                 .authorizeRequests()
                 .antMatchers(
                         "/user/test",
                         "/test",
                         SecurityConstants.DEFAULT_UNAUTHENTICATION_URL,
+                        SecurityConstants.DEFAULT_VALIDATE_CODE_URL_PREFIX+"/*",
                         securityProperties.getBrowser().getLoginPage()
                 ).permitAll() //指明哪些路径可以通过
                 .anyRequest()
@@ -67,5 +96,17 @@ public class BrowerSecurityConfig extends WebSecurityConfigurerAdapter {
         //关闭默认的csrf认证
         http.csrf().disable();*/
 
+    }
+
+    /**
+     * 记住我储存的dao
+     * @return
+     */
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+        tokenRepository.setDataSource(dataSource);
+		//tokenRepository.setCreateTableOnStartup(true); //自动创建表  只能执行一次
+        return tokenRepository;
     }
 }
